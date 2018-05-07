5315205046b2f8bbe4f2c1958a2dda6422759ea2
Dameon1 <dameonmedoza@gmail.com>
2018-05-07 14:24:04 -0700

    bcrypt added

---
+++ models/users.js
@@ -0,0 +1,66 @@
+'use strict';
+
+//const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+// ===== Define UserSchema & UserModel =====
+const UserSchema = new mongoose.Schema({
+  fullname: { type: String },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+UserSchema.set('toObject', {
+  transform: function (doc, ret) {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+    delete ret.password;
+  }
+});
+
+UserSchema.methods.validatePassword = function (password) {
+  return password === this.password;
+};
+
+
+UserSchema.methods.validatePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+UserSchema.statics.hashPassword = function (password) {
+  return bcrypt.hash(password, 10);
+};
+
+
+
+
+const User = mongoose.model('User',UserSchema);
+
+module.exports = User;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
