c231eb26788f3cf52bcc3c39e0f22e6f31fe1a95
Dameon1 <dameonmedoza@gmail.com>
2018-05-11 12:22:08 -0700

    testA heroku

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/noteful';
 exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI ||'mongodb://dev:dev@ds111430.mlab.com:11430/noteful-test';
 // || 'mongodb://dev:dev@ds111430.mlab.com:11430/noteful-test' || process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test';
 
-
+ 
 
 exports.JWT_SECRET = process.env.JWT_SECRET || 'secret';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
